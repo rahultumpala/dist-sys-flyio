d15c08e78d945405b81be51bbd35372a05d885f5
rahultumpala <rhltumpala@gmail.com>
2024-07-29 16:07:08 +0530

    draft

--- kafka-style-log/single-node/main.go
+++ kafka-style-log/single-node/main.go
@@ -9,13 +9,22 @@ import (
 
 var node *maelstrom.Node
 
+/*
+----- STRUCT -----
+linked list
+*/
+
 type Event struct {
-	offset int
-	value  int
+	offset float64
+	value  float64
+	next   *Event
 }
 
-func (event Event) to_list() []int {
-	result := make([]int, 0)
+var msgs_tail map[string]Event = make(map[string]Event)
+var msgs_head map[string]Event = make(map[string]Event)
+
+func (event Event) to_list() []float64 {
+	result := make([]float64, 0)
 	result = append(result, event.offset)
 	result = append(result, event.value)
 	return result
@@ -37,26 +46,37 @@ func get_body_from_msg(msg maelstrom.Message) map[string]any {
 	return body
 }
 
+func get_recent_msg(key string) (Event, bool) {
+	event, ok := msgs_tail[key]
+	if !ok {
+		return Event{}, false
+	}
+	return event, true
+}
+
 /*
 ------------------
    RPC Handlers
 ------------------
 */
 
-var offset int = 0
-var msgs map[string][]Event = make(map[string][]Event)
+var offset float64 = 0
+var committed_offsets map[string]float64 = make(map[string]float64)
 
 func handle_send(msg maelstrom.Message) error {
 	body := get_body_from_msg(msg)
 	var key string = body["key"].(string)
-	msg_val := body["msg"].(int)
+	msg_val := body["msg"].(float64)
 
-	val, ok := msgs[key]
+	val, ok := get_recent_msg(key)
+	curr_event := Event{offset, msg_val, nil}
 	if !ok {
-		val = make([]Event, 0)
+		msgs_head[key] = curr_event
+		msgs_tail[key] = curr_event
+	} else {
+		val.next = &curr_event
+		msgs_tail[key] = curr_event
 	}
-	val = append(val, Event{offset, msg_val})
-	msgs[key] = val
 
 	reply := make(map[string]any)
 	reply["type"] = "send_ok"
@@ -69,19 +89,21 @@ func handle_send(msg maelstrom.Message) error {
 func handle_poll(msg maelstrom.Message) error {
 	body := get_body_from_msg(msg)
 
-	var offsets map[string]float64 = body["offsets"].(map[string]float64)
-	var results map[string][][]int = make(map[string][][]int)
+	var offsets map[string]any = body["offsets"].(map[string]any)
+	var results map[string][][]float64 = make(map[string][][]float64)
 
 	for key, req_offset := range offsets {
-		for _, event := range msgs[key] {
-			if event.offset >= int(req_offset) {
+		event := msgs_head[key]
+		for event.next != nil {
+			if event.offset >= req_offset.(float64) {
 				result, ok := results[key]
 				if !ok {
-					result = make([][]int, 0)
+					result = make([][]float64, 0)
 				}
 				result = append(result, event.to_list())
 				results[key] = result
 			}
+			event = *event.next
 		}
 	}
 
@@ -92,10 +114,49 @@ func handle_poll(msg maelstrom.Message) error {
 	return node.Reply(msg, reply)
 }
 
+func handle_commit_offsets(msg maelstrom.Message) error {
+	body := get_body_from_msg(msg)
+	var offsets map[string]any = body["offsets"].(map[string]any)
+
+	for key, off_float := range offsets {
+		commit_offset := off_float.(float64)
+		event := msgs_head[key]
+		if commit_offset == msgs_tail[key].offset {
+			delete(msgs_head, key)
+			delete(msgs_tail, key)
+		} else {
+			for event.next != nil {
+				if event.offset <= commit_offset {
+					next := *event.next
+					event.next = nil
+
+					msgs_head[key] = next
+					event = next
+				}
+			}
+		}
+		committed_offsets[key] = commit_offset
+	}
+
+	var reply map[string]string = make(map[string]string)
+	reply["type"] = "commit_offsets_ok"
+
+	return node.Reply(msg, reply)
+}
+
+func handle_list_committed_offsets(msg maelstrom.Message) error {
+	var reply map[string]any = make(map[string]any)
+	reply["type"] = "list_committed_offsets_ok"
+	reply["offsets"] = committed_offsets
+	return node.Reply(msg, reply)
+}
+
 func main() {
 	node = maelstrom.NewNode()
 	node.Handle("send", handle_send)
 	node.Handle("poll", handle_poll)
+	node.Handle("commit_offsets", handle_commit_offsets)
+	node.Handle("list_committed_offsets", handle_list_committed_offsets)
 	err := node.Run()
 	if err != nil {
 		log.Fatal(err)
